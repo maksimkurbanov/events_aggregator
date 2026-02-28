0288391a71c045d16eb48b7a82a0d48bfb3f38fc
Maksim <mkurbanov2@gmail.com>
2026-02-28 13:48:29 +0300

    Formatting

--- src/main.py
+++ src/main.py
@@ -14,9 +14,7 @@ def healthcheck():
 
 
 async def main():
-    config = uvicorn.Config(
-        app=app, host="0.0.0.0", port=dev_settings.SERVER_PORT
-    )
+    config = uvicorn.Config(app=app, host="0.0.0.0", port=dev_settings.SERVER_PORT)
     server = uvicorn.Server(config)
     await server.serve()
 
